e309956b082a864df849155d199ea79e771f605b
Loïc <loic.le-loarer@polytechnique.org>
2022-11-19 15:15:52 +0000

    Fix exception when retrying download, add more try around launch_download to avoid the main loop to exit when an exception occurs

--- watch_download.py
+++ watch_download.py
@@ -9,16 +9,27 @@ import os
 def launch_download(dir):
     url    = (dir/"url").read_text()
     format = (dir/"format").read_text()
-    print(f"Start download of {url} in '{format}' format in {dir}")
-    (dir/"commit").rename(dir/"inprogress")
+    try:
+        (dir/"commit").rename(dir/"inprogress")
+    except FileNotFoundError:
+        try:
+            (dir/"retry").rename(dir/"inprogress")
+        except FileNotFoundError:
+            return
+    current_retry = (dir/"inprogress").read_text()
+    current_retry = 0 if current_retry == "" else int(current_retry)
+    print(f"Start download of {url} in '{format}' format in {dir}: try #{current_retry}")
     ret = sp.run(["echo", "yt-dlp", url] + ([ "--format", format] if format else []), cwd=dir)
     ret = sp.run(["yt-dlp", url] + ([ "--format", format] if format else []), cwd=dir)
     if ret.returncode == 0:
         (dir/"inprogress").rename(dir/"done")
+    elif current_retry >= 10:
+        (dir/"inprogress").rename(dir/"failed")
     else:
         (dir/"inprogress").rename(dir/"retry")
+        (dir/"retry").write_text(str(current_retry+1))
     return
-            
+
 
 def main():
     # watch current dir for tmp dir creation with commit file
@@ -27,7 +38,14 @@ def main():
             if not dir.is_dir(): continue
             if (dir / "commit").is_file() or\
                 (dir/"retry").is_file():
-                launch_download(dir)
+                try:
+                    launch_download(dir)
+                except Exception as e:
+                    print(e)
+                    try:
+                        (dir/"inprogress").rename(dir/"retry")
+                    except:
+                        pass
         sleep(10)
     return 0
 
